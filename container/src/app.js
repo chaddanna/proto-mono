5be4bb09bd6d0dad7bf216ad12f25ac893bd7440
chaddanna <chad@formulated.io>
2021-07-29 10:33:24 -0600

    feat(Container): Compose MountedApp with DashboardApp for cleanup

--- container/src/app.js
+++ container/src/app.js
@@ -2,12 +2,14 @@ import React from "react";
 import { mount as dashboardMount } from "dashboard/DashboardApp";
 import { MountedApp } from "./components/mounted-app";
 
+const MountedDashboard = () => <MountedApp mountFn={dashboardMount} />;
+
 export function App() {
   return (
     <div>
       <h1>[Container] Probably some navigation component here</h1>
       <hr />
-      <MountedApp mountFn={dashboardMount} />
+      <MountedDashboard />
     </div>
   );
 }
