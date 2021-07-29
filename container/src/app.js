8651362f9ae16c648607ee5befb01ceaa37e7810
chaddanna <chad@formulated.io>
2021-07-29 15:26:24 -0600

    fix: Make container change to run deploy

--- container/src/app.js
+++ container/src/app.js
@@ -7,7 +7,7 @@ const MountedDashboard = () => <MountedApp mountFn={dashboardMount} />;
 export function App() {
   return (
     <div>
-      <h1>[Container] Probably some navigation component here</h1>
+      <h1>[Container] NavigationComponent here</h1>
       <hr />
       <MountedDashboard />
     </div>
